18e362a7634d1e629aaf2e384d086a01cb4af823
qwh <15806293089@163.com>
2022-11-01 15:31:47 +0800

    实现 proxyRefs 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,7 +2,7 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-01 14:29:54
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-01 15:15:46
+ * @LastEditTime: 2022-11-01 15:30:18
  * @FilePath: /mini-vue-study/src/reactivity/ref.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ class RefImpl {
     _value = ''
     dep = new Set()
     private rawValue;
-    public __v_isRef:any = true
+    public __v_isRef: any = true
     constructor(value: any) {
         this.rawValue = value
         this._value = convert(value)
@@ -33,23 +33,39 @@ class RefImpl {
         }
     }
 }
-function trackRefValue(ref:any){
+function trackRefValue(ref: any) {
     if (isTracking()) {
         trackEffect(ref.dep)
     }
 }
 
-function convert(value:any){
-    return isObject(value)? reactive(value) : value
+function convert(value: any) {
+    return isObject(value) ? reactive(value) : value
 }
 export function ref(value: any) {
     return new RefImpl(value);
 }
 
-export function isRef(ref:any){
-     return !!ref['__v_isRef']
+export function isRef(ref: any) {
+    return !!ref['__v_isRef']
 }
 
-export function unRef(ref: any){
-     return isRef(ref)? ref.value: ref
+export function unRef(ref: any) {
+    return isRef(ref) ? ref.value : ref
+}
+
+export function proxyRefs(objectWithRefs: any) {
+    return new Proxy(objectWithRefs, {
+        get(target, key) {
+            return unRef(Reflect.get(target, key))
+        },
+        set(target, key, value) {
+            //原值是 ref，新值不是,那就给到它的.value.其他的直接赋值
+            if (isRef(target[key]) && !isRef(value)) {
+                return target[key].value = value
+            } else {
+                return Reflect.set(target, key, value)
+            }
+        }
+    })
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -2,13 +2,13 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-01 14:29:26
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-01 15:15:53
+ * @LastEditTime: 2022-11-01 15:30:33
  * @FilePath: /mini-vue-study/src/reactivity/tests/ref.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { effect } from "../effect";
 import { reactive } from "../reactive";
-import { isRef, ref, unRef } from "../ref";
+import { isRef, proxyRefs, ref, unRef } from "../ref";
 describe("ref", () => {
   it("happy path", () => {
     const a = ref(1);
@@ -61,6 +61,27 @@ describe("ref", () => {
     const a = ref(1);
     expect(unRef(a)).toBe(1);
     expect(unRef(1)).toBe(1);
+  });
+
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaohong",
+    };
+
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("xiaohong");
+
+    proxyUser.age = 20;
+
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
   });
 
 });
