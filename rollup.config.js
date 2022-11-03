a01e52c3df79749bbed8d3d53a037624f8001e09
qwh <15806293089@163.com>
2022-11-03 16:54:04 +0800

    使用 rollup 打包库

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-03 15:05:35
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-03 16:49:32
+ * @LastEditTime: 2022-11-03 16:53:42
  * @FilePath: /mini-vue-study/rollup.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import typescript from '@rollup/plugin-typescript';
-// const typescript = require('@rollup/plugin-typescript')
+// const1 typescript = require('@rollup/plugin-typescript')
 
 export default {
     input: './src/index.ts' ,//入口
