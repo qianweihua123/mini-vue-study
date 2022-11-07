d516aac927c1ccff1479037662dfc78f33105cd8
qwh <15806293089@163.com>
2022-11-07 14:55:28 +0800

    实现注册事件功能

--- example/helloword/App.js
+++ example/helloword/App.js
@@ -2,7 +2,7 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-03 15:51:17
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-05 19:46:00
+ * @LastEditTime: 2022-11-07 14:40:20
  * @FilePath: /mini-vue-study/example/helloword/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,10 @@ export const App = {
     // ui
     return h("div", {
         id: 'root',
-        class: ["red", "hard"]
+        class: ["red", "hard"],
+        onClick(){
+          console.log('click')
+        }
       },
       // [h("p", {
       //   class: "red"
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -5,7 +5,7 @@ import { isObject } from './../reactivity/shared/index';
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-03 10:19:35
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-06 23:35:27
+ * @LastEditTime: 2022-11-07 14:52:50
  * @FilePath: /mini-vue-study/src/runtime-core/renderer.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,7 +23,7 @@ function patch(vnode: any, container: any) {
 
    //如果是组件的话 vnode 上的 type 是一个对象，如果是元素的话 type 是 string(div)
    //我们先处理组件 processComponent
-   const {shapeFlag} = vnode
+   const { shapeFlag } = vnode
    if (shapeFlag & ShapeFlags.ELEMENT) {
       processElement(vnode, container)
    } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
@@ -41,13 +41,13 @@ function mountComponent(initialVnode: any, container: any) {
    setupComponent(instance) //上面这些都是把组件的信息收集起来
 
    //下面的这个是开箱得到虚拟节点树
-   setupRenderEffect(instance,initialVnode, container)
+   setupRenderEffect(instance, initialVnode, container)
 }
 
 
 
 
-function setupRenderEffect(instance: any,vnode:any, container: any) {
+function setupRenderEffect(instance: any, vnode: any, container: any) {
    //这个 subTree 其实是一个虚拟节点树，
    /**
    * @description 描述
@@ -79,7 +79,7 @@ function mountElement(vnode: any, container: any) {
    //创建挂载元素
    //将创建的真实元素存储到虚拟节点的 el上
    const el = (vnode.el = document.createElement(vnode.type))
-   const { children,shapeFlag } = vnode
+   const { children, shapeFlag } = vnode
    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       el.textContent = children
    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
@@ -88,10 +88,17 @@ function mountElement(vnode: any, container: any) {
 
    const { props } = vnode
    for (const key in props) {
-      el.setAttribute(key, props[key])
+      console.log(key);
+      const isOn = (key:string) => /^on[A-Z]/.test(key)
+      if (isOn(key)) {
+         const event = key.slice(2).toLocaleLowerCase()
+         el.addEventListener(event,props[key])
+      } else {
+         el.setAttribute(key, props[key])
+      }
+
    }
    container.appendChild(el)
-
 }
 
 function mountChildren(vnode: any, container: any) {
